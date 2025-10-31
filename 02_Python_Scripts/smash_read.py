2ec0966c3966c826ab8717f1f21e2990d1b3c45f
sebastianostro <sebastian.ostrowski@hotmail.com>
2025-10-31 11:18:30 +0100

    Fixed issues in calculate_rapidity and calculate_invariant_mass functions

--- 02_Python_Scripts/smash_read.py
+++ 02_Python_Scripts/smash_read.py
@@ -33,10 +33,10 @@ def read_smash_file(file_path):
 ## Function to calculate rapidity values for data in a DataFrame
 ### Input: 
 # "df" is the original Pandas DataFrame without rapidity information
-# "col_no_energy" is the column number that contains the energy (aka p0, default value is "4" in SMASH output)
-# "col_no_beam" is the column number that contains the momentum in beam direction (aka pz for z-beam, default value is "7" in SMASH output)
+# "col_no_energy" is the column number that contains the energy (aka p0, default value is "5" in SMASH output)
+# "col_no_beam" is the column number that contains the momentum in beam direction (aka pz for z-beam, default value is "8" in SMASH output)
 ### Output: Original DataFrame enriched by a column containing the rapidity values (named 'y')
-def calculate_rapidity(df, col_no_energy=4, col_no_beam=7):
+def calculate_rapidity(df, col_no_energy=5, col_no_beam=8):
     p0 = df[col_no_energy]  # Energy column
     pz = df[col_no_beam]  # pz column
     df['y'] = 0.5 * np.log((p0 + pz) / (p0 - pz))
@@ -45,19 +45,18 @@ def calculate_rapidity(df, col_no_energy=4, col_no_beam=7):
 ## Function to calculate invariant mass for data in a DataFrame
 ### Input: 
 # "df" is the original Pandas DataFrame without invariant mass information
-# "col_no_energy" is the column number that contains the energy (aka p0, default value is "4" in SMASH output)
-# "col_no_px" is the column number that contains the momentum in x direction (default value is "5" in SMASH output)
-# "col_no_py" is the column number that contains the momentum in y direction (default value is "6" in SMASH output)
-# "col_no_pz" is the column number that contains the momentum in z direction (default value is "7" in SMASH output)
+# "col_no_energy" is the column number that contains the energy (aka p0, default value is "5" in SMASH output)
+# "col_no_px" is the column number that contains the momentum in x direction (default value is "6" in SMASH output)
+# "col_no_py" is the column number that contains the momentum in y direction (default value is "7" in SMASH output)
+# "col_no_pz" is the column number that contains the momentum in z direction (default value is "8" in SMASH output)
 ### Output: Original DataFrame enriched by a column containing the invariant mass values (named 'm_inv')
-def calculate_invariant_mass(df, col_no_energy=4, col_no_px=5, col_no_py=6, col_no_pz=7):
+def calculate_invariant_mass(df, col_no_energy=5, col_no_px=6, col_no_py=7, col_no_pz=8):
     p0 = df[col_no_energy]
     px = df[col_no_px]
     py = df[col_no_py]
     pz = df[col_no_pz]
     df['m_inv'] = (p0**2 - (px**2 + py**2 + pz**2))
     return df
-
 
 # Main script execution
 # Read the SMASH file and store the data in a DataFrame
@@ -66,10 +65,10 @@ smash_data = read_smash_file(smash_file)
 if smash_data is not None:
     # Display the first few rows of the DataFrame
     print(smash_data.head())
-    smash_data = calculate_rapidity(smash_data)
-    smash_data = calculate_invariant_mass(smash_data)
-    print(smash_data.head())
-    print(smash_data[smash_data['m_inv']>0].head())  # Display first few rows with valid invariant mass
+    smash_data_enriched = calculate_rapidity(smash_data)
+    smash_data_enriched = calculate_invariant_mass(smash_data)
+    print(smash_data_enriched.head())
+    print(smash_data_enriched[smash_data_enriched['m_inv']<0].head())  # Display first few rows with valid invariant mass
 else:
     print("Failed to read SMASH data.")
 
