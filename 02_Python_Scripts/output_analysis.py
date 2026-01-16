60c871fc0ee8288bb8b4ac4761fbdeb247b5c383
Sebastian Ostrowski <sebastian.ostrowski@hotmail.com>
2026-01-16 15:48:11 +0100

    No relevant changes

--- 02_Python_Scripts/output_analysis.py
+++ 02_Python_Scripts/output_analysis.py
@@ -14,7 +14,7 @@ import plotting as plot
 # CONSTANTS AND SETTINGS
 # -----------------------------
 # Define file information (paths, names)
-data_dir_name = 'Dilepton_Output_Std_Nevents_10000_OutInt_NaN/' # Example data subdirectory
+data_dir_name = 'Dilepton_Output_Std_Nevents_5_OutInt_NaN/' # Example data subdirectory
 file_name = 'Dileptons.oscar'  # Example SMASH output file name
 
 # -----------------------------
@@ -32,8 +32,9 @@ short_dilepton_data = io_smash.aggregate_dilepton_pairs(full_dilepton_data)
 #smash_data_enriched = sof.calculate_rapidity(smash_data)
 dilepton_data_enriched = sof.calculate_invariant_mass(short_dilepton_data, col_energy="p0", col_px="px", col_py="py", col_pz="pz")
 dilepton_data_enriched = sof.enrich_dilepton_with_parent(dilepton_data_enriched)
-dilepton_data_enriched = sof.adjust_shining_weights(dilepton_data_enriched)
 
-plot.plot_hist_dilepton_invariant_mass(dilepton_data_enriched, save_figure=True, file_name="Hist_np_1.5GeV_10000events.png", in_max_gap_bins=4, in_bins=40)
+print((dilepton_data_enriched["p_pdg_id"]==-1111).sum())
+
+#plot.plot_hist_dilepton_invariant_mass(dilepton_data_enriched, save_figure=True, file_name="Hist_np_1.5GeV_10000events.png", in_max_gap_bins=4, in_bins=40)
 
 # End of script
